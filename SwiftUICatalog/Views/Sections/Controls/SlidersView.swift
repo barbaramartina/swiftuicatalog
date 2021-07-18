27d86cf6800bc9cd219111e3d17523036aa67d26
barbara <barbara@komoot.de>
2021-07-18 22:43:17 +0200

    3 examples of sliders

--- SwiftUICatalog/Views/Sections/Controls/SlidersView.swift
+++ SwiftUICatalog/Views/Sections/Controls/SlidersView.swift
@@ -32,8 +32,81 @@ import SwiftUI
 /// OFFICIAL DOCUMENTATION https://developer.apple.com/documentation/swiftui/slider
 /// 
 struct SlidersView: View {
+    
+    // MARK: - Properties
+    
+    @State private var grams1 = 15.0
+    @State private var grams2 = 15.0
+    @State private var grams3 = 15.0
+    @State private var isEditing1 = false
+    @State private var isEditing2 = false
+    @State private var isEditing3 = false
+
+    // MARK: - Body
+    
+    
     var body: some View {
-        Text("Sliders in SwiftUI")
+
+        List {
+            HeaderView(title: "Slider with continued values")
+            VStack {
+                Slider(
+                    value: $grams1,
+                    in: 0...1000,
+                    onEditingChanged: { editing in
+                        isEditing1 = editing
+                    }
+                )
+                .padding(30)
+                Text("\(grams1)")
+                    .foregroundColor(isEditing1 ? .blue : .black)
+            }
+            .padding(10)
+            // end of v stack
+            
+            HeaderView(title: "Slider with steps")
+            VStack {
+                Slider(
+                    value: $grams2,
+                    in: 0...1000,
+                    step: 20,
+                    onEditingChanged: { editing in
+                        isEditing2 = editing
+                    }
+                )
+                .padding(30)
+                Text("\(grams2)")
+                    .foregroundColor(isEditing2 ? .blue : .black)
+            }
+            .padding(10)
+            // end of v stack
+            
+            HeaderView(title: "Slider with VoiceOver Label & min / max values")
+            VStack {
+                Slider(value: $grams3,
+                       in: 0...1000,
+                       onEditingChanged: { editing in
+                        isEditing3 = editing
+                       },
+                       minimumValueLabel: Label(
+                        title: { Text("50") },
+                        icon: { Image(systemName: "circle") }
+                       ),
+                       maximumValueLabel: Label(
+                        title: { Text("900") },
+                        icon: { Image(systemName: "circle") }
+                        
+                       ),
+                       label: {
+                        Text("This is a slider for grams")
+                       })
+                    .padding(30)
+            }
+            .padding(10)
+            // end of v stack
+
+        }
+        // end of LIST
     }
 }
 
