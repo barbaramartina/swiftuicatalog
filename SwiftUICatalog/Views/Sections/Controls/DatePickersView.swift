0041ae2555d8c6db6e30fbd17b8ab64f9c9d7420
barbara <barbara@komoot.de>
2021-08-08 11:43:34 +0200

    taking datepickers

--- SwiftUICatalog/Views/Sections/Controls/DatePickersView.swift
+++ SwiftUICatalog/Views/Sections/Controls/DatePickersView.swift
@@ -4,7 +4,7 @@
 //
 // MIT License
 //
-// Copyright (c) 2021 { YOUR NAME HERE 🏆 }
+// Copyright (c) 2021 Barbara Martina Rodeker
 //
 // Permission is hereby granted, free of charge, to any person obtaining a copy
 // of this software and associated documentation files (the "Software"), to deal
