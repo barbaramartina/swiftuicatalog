2de8d3ea04d7d1ed570223a4563cf645a1acfe62
Ali ghayeni <alighaeni@gmail.com>
2021-08-15 11:44:34 +0200

    Add Pickers view samples

--- SwiftUICatalog/Views/Sections/Controls/PickersView.swift
+++ SwiftUICatalog/Views/Sections/Controls/PickersView.swift
@@ -33,8 +33,115 @@ import SwiftUI
 /// OFFICIAL DOCUMENTATION https://developer.apple.com/documentation/swiftui/picker
 ///
 struct PickersView: View {
+    
+    @State private var selectedFlavor = Flavor.strawberry
+    @State private var selectedTopping = Topping.nuts
+    @State var suggestedTopping: Topping = .cookies
+    
     var body: some View {
-        Text("Pickers in SwiftUI")
+        ScrollView{
+            Group {
+                VStack{
+                    Text("Pickers in SwiftUI").fontWeight(.heavy)
+                    Text("You create a picker by providing a selection binding, a label, and the content for the picker to display. Set the selection parameter to a bound property that provides the value to display as the current selection. Set the label to a view that visually describes the purpose of selecting content in the picker, and then provide the content for the picker to display.")
+                        .fontWeight(.light)
+                        .padding()
+                    Spacer()
+                    Text("Selected flavor: \(selectedFlavor.rawValue)")
+                    Picker("Flavor", selection: $selectedFlavor) {
+                        Text("Chocolate").tag(Flavor.chocolate)
+                        Text("Vanilla").tag(Flavor.vanilla)
+                        Text("Strawberry").tag(Flavor.strawberry)
+                        Label("Star Falavor",
+                              systemImage: "staroflife.fill")
+                            .font(.subheadline)
+                            .tag(Flavor.star)
+                    }
+                    .background(Color.yellow)
+                }
+            }
+            Group {
+                VStack{
+                    Text("Iterating Over a Picker’s Options").fontWeight(.heavy)
+                    Text("Choose your flavor and check the suggested topping options.")
+                        .fontWeight(.light)
+                        .padding()
+                    Text("suggestedTopping: \(suggestedTopping.rawValue)")
+                    /**
+                     To provide selection values for the Picker without explicitly listing each option, you can create the picker with a ForEach construct, like this:
+                     
+                     Picker("Flavor", selection: $selectedFlavor) {
+                         ForEach(Flavor.allCases) { flavor in
+                             Text(flavor.rawValue.capitalized)
+                         }
+                     }
+                     
+                     In this case, ForEach automatically assigns a tag to the selection views, using each option’s id, which it can do because Flavor conforms to the Identifiable protocol.
+
+                     On the other hand, if the selection type doesn’t match the input to the ForEach, you need to provide an explicit tag. The following example shows a picker that’s bound to a Topping type, even though the options are all Flavor instances. Each option uses tag(_:) to associate a topping with the flavor it displays.
+                     */
+                    Picker("Suggest a topping for:", selection: $suggestedTopping) {
+                        ForEach(Flavor.allCases) { flavor in
+                            Text(flavor.rawValue.capitalized)
+                                .tag(flavor.suggestedTopping)
+                        }
+                    }
+                    .background(Color.green)
+                }
+            }
+            Group {
+                VStack{
+                    Text("Styling Pickers").fontWeight(.heavy)
+                    Text("You can customize the appearance and interaction of pickers by creating styles that conform to the PickerStyle protocol. You create your own style or use one of the styles provided by SwiftUI, like segmented or menu.")
+                        .fontWeight(.light)
+                        .padding()
+                    VStack {
+                        Text("Selected flavor: \(selectedFlavor.rawValue)")
+                        Text("Selected topping: \(selectedTopping.rawValue)")
+                        Picker(selection: $selectedFlavor, label: Text("Flavor")) {
+                            ForEach(Flavor.allCases, id: \.self) {
+                                Text($0.rawValue.capitalized)
+                            }
+                        }
+                        Picker(selection: $selectedTopping, label: Text("Topping")) {
+                            ForEach(Topping.allCases, id: \.self) {
+                                Text($0.rawValue.capitalized)
+                            }
+                        }
+                        .pickerStyle(SegmentedPickerStyle())
+                        Spacer()
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+            }
+        }
+    }
+}
+
+enum Flavor: String, CaseIterable, Identifiable {
+    case chocolate
+    case vanilla
+    case strawberry
+    case star
+    var id: String { self.rawValue }
+}
+
+enum Topping: String, CaseIterable, Identifiable {
+    case nuts
+    case cookies
+    case blueberries
+    case other
+    var id: String { self.rawValue }
+}
+
+extension Flavor {
+    var suggestedTopping: Topping {
+        switch self {
+        case .chocolate: return .nuts
+        case .vanilla: return .cookies
+        case .strawberry: return .blueberries
+        case .star: return .other
+        }
     }
 }
 
