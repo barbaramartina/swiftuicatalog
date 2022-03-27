9136a428868a30d0472fde298213f39b6d1124db
barbara <barbara@komoot.de>
2022-03-27 17:57:49 +0200

    Adding example of views compositions
    Reusable component: Image with overlay

--- SwiftUICatalog/Views/Composed Components/ImageWithOverlayView.swift
+++ SwiftUICatalog/Views/Composed Components/ImageWithOverlayView.swift
@@ -7,14 +7,37 @@
 
 import SwiftUI
 
-struct ImageWithOverlayView: View {
+///
+/// Composing views can be achieved by parametrizing
+/// A View with another View
+///
+/// In this case you can pass any type of view to
+/// be presented as an overlay on top of an image
+/// 
+struct ImageWithOverlayView<Overlay: View>: View {
+    
+    let image: String
+    let overlay: Overlay
+    let verticalAlignment: VerticalAlignment
+    let horizontalAligment: HorizontalAlignment
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        ZStack(alignment: Alignment(horizontal: .leading,
+                                    vertical: .top)) {
+            Image(image)
+                .resizable()
+                .scaledToFit()
+            overlay
+                .padding()
+        }
     }
 }
 
 struct ImageWithOverlayView_Previews: PreviewProvider {
     static var previews: some View {
-        ImageWithOverlayView()
+        ImageWithOverlayView(image: "cocker",
+                             overlay: Text("Nice puppy :)"),
+                             verticalAlignment: .top,
+                             horizontalAligment: .leading)
     }
 }
