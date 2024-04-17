23705da62b32e52385d45aecb2cefe34b0e145d3
barbara <barbararodeker@gmail.com>
2024-04-17 21:16:55 +0200

    Adding Style with basic UI configuration

---
+++ SwiftUICatalog/Views/Style/Style.swift
@@ -0,0 +1,23 @@
+//
+//  Style.swift
+//  SwiftUICatalog
+//
+//  Created by Barbara Personal on 2024-04-13.
+//
+
+import Foundation
+
+/// UI spacing, margings, colors, used accross the app
+struct Style {
+    /// vertical view separation
+    enum VerticalPadding: CGFloat {
+        case small = 8
+        case medium = 16
+        case large = 24
+    }
+    /// horizontal view separation
+    enum HorizontalPadding: CGFloat {
+        case small = 8
+        case medium = 16
+    }
+}
