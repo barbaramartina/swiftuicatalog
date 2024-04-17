043c9a2c935827ed47ddb293efa91dc87f517d14
barbara <barbararodeker@gmail.com>
2024-04-17 21:18:15 +0200

    naming: more clarity

--- SwiftUICatalog/Views/Reusable Custom Views/VerticalAlignmentPicker.swift
+++ SwiftUICatalog/Views/Reusable Custom Views/VerticalAlignmentPicker.swift
@@ -10,11 +10,13 @@ import SwiftUI
 
 struct VerticalAlignmentPicker: View {
     
+    @Binding var selection: VerticalAlignment
+    
     /// alignments for stack view elements
     private let verticalAlignments: [VerticalAlignment] = [.top, .bottom, .center, .firstTextBaseline, .lastTextBaseline]
 
     var body: some View {
-        Picker(selection: $verticalAlignment, label: Text("alignment")) {
+        Picker(selection: $selection, label: Text("alignment")) {
             ForEach(verticalAlignments, id: \.self) {
                 Text($0.description)
             }
