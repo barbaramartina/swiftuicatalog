cc4a5faa8b96fe2d8085575658bc1b938be60c4f
barbaramartina <barbararodeker@gmail.com>
2024-06-09 13:40:55 +0200

    Improving animation examples

--- SwiftUICatalog/Views/Sections/Drawing and Animations/TransitionsAnimationsView.swift
+++ SwiftUICatalog/Views/Sections/Drawing and Animations/TransitionsAnimationsView.swift
@@ -13,116 +13,132 @@ struct TransitionsAnimationsView: View, Comparable {
     
     let id: String = "TransitionsAnimationsView"
     
-    @State private var animate1 = false
-    @State private var animate2 = false
-    @State private var animate3 = false
-    @State private var animate4 = false
-    @State private var animate5 = false
-    @State private var animate6 = false
-    @State private var animate7 = false
-    @State private var animate8 = false
-    @State private var animate9 = false
+    @State private var animate1 = false { didSet { if animate1 == true { resetFlags(indexes: [2,3,4,5,6,7,8,9]) } } }
+    @State private var animate2 = false { didSet { if animate2 == true { resetFlags(indexes: [1,3,4,5,6,7,8,9]) } } }
+    @State private var animate3 = false { didSet { if animate3 == true { resetFlags(indexes: [1, 2,4,5,6,7,8,9]) } } }
+    @State private var animate4 = false { didSet { if animate4 == true { resetFlags(indexes: [1,2,3,5,6,7,8,9]) } } }
+    @State private var animate5 = false { didSet { if animate5 == true { resetFlags(indexes: [1,2,3,4,6,7,8,9]) } } }
+    @State private var animate6 = false { didSet { if animate6 == true { resetFlags(indexes: [1,2,3,4,5,7,8,9]) } } }
+    @State private var animate7 = false { didSet { if animate7 == true { resetFlags(indexes: [1,2,3,4,5,6,8,9]) } } }
+    @State private var animate8 = false { didSet { if animate8 == true { resetFlags(indexes: [1,2,3,4,5,6,7,9]) } } }
+    @State private var animate9 = false { didSet { if animate9 == true { resetFlags(indexes: [1,2,3,4,5,6,7,8]) } } }
     
     // MARK: - Body
     
     
     var body: some View {
-        VStack(alignment: .leading) {
-            HStack {
-                Text("Tap each button to see how the animation mentioned would look like, and then tap it again to see how the animation reverses.")
-                    .fontWeight(.light)
-                Spacer()
+        PageContainer(content: ScrollView {
+            VStack(alignment: .leading) {
+                HStack {
+                    Text("Tap each button to see how the animation mentioned would look like, and then tap it again to see how the animation reverses.")
+                        .fontWeight(.light)
+                    Spacer()
+                }
+                
+                HStack {
+                    button1
+                    button2
+                    button3
+                    button4
+                }
+                HStack {
+                    button5
+                    button6
+                }
+                HStack {
+                    button7
+                    button8
+                }
+                HStack {
+                    button9
+                }
+                
+                if animate1 {
+                    Image(systemName: "cloud.circle.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .transition(.slide)
+                        .padding(100)
+                }
+                
+                if animate2 {
+                    Image(systemName: "cloud.circle")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .transition(.opacity)
+                        .padding(100)
+                }
+                
+                if animate3 {
+                    Image(systemName: "moon.stars.circle")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .transition(.moveAndFade)
+                        .padding(100)
+                }
+                
+                if animate4 {
+                    Image(systemName: "moon.stars.circle.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .transition(.scale)
+                        .padding(100)
+                }
+                if animate5 {
+                    Image(systemName: "moon.haze.circle.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .transition(.move(edge: .bottom))
+                        .padding(.vertical, 100)
+                }
+                if animate6 {
+                    Image(systemName: "sparkles")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .transition(.leadingBottom)
+                        .padding(100)
+                }
+                if animate7 {
+                    Image(systemName: "moon.stars")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .transition(.trailingBottom)
+                        .padding(100)
+                }
+                if animate8 {
+                    Image(systemName: "moon.stars.fill")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .transition(.push(from: .bottom))
+                        .padding(100)
+                }
+                if animate9 {
+                    Image(systemName: "moon.stars.circle")
+                        .resizable()
+                        .frame(width: 100, height: 100)
+                        .transition(.blurReplace)
+                        .padding(100)
+                }
             }
-            
-            HStack {
-                button1
-                button2
-                button3
-                button4
-            }
-            HStack {
-                button5
-                button6
-            }
-            HStack {
-                button7
-                button8
-            }
-            HStack {
-                button9
-            }
-            
-            if animate1 {
-                Image("corgie-love")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .transition(.slide)
-                    .padding(100)
-            }
-            
-            if animate2 {
-                Image("corgie-love")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .transition(.opacity)
-                    .padding(100)
-            }
-            
-            if animate3 {
-                Image("corgie-love")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .transition(.moveAndFade)
-                    .padding(100)
-            }
-            
-            if animate4 {
-                Image("corgie-love")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .transition(.scale)
-                    .padding(100)
-            }
-            if animate5 {
-                Image("corgie-love")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .transition(.move(edge: .bottom))
-                    .padding(.vertical, 100)
-            }
-            if animate6 {
-                Image("corgie-love")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .transition(.leadingBottom)
-                    .padding(100)
-            }
-            if animate7 {
-                Image("corgie-love")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .transition(.trailingBottom)
-                    .padding(100)
-            }
-            if animate8 {
-                Image("corgie-love")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .transition(.push(from: .bottom))
-                    .padding(100)
-            }
-            if animate9 {
-                Image("corgie-love")
-                    .resizable()
-                    .frame(width: 100, height: 100)
-                    .transition(.blurReplace)
-                    .padding(100)
+        })
+        
+    }
+    
+    private func resetFlags(indexes: [Int]) {
+        indexes.forEach { index in
+            switch index {
+            case 1: animate1 = false
+            case 2: animate2 = false
+            case 3: animate3 = false
+            case 4: animate4 = false
+            case 5: animate5 = false
+            case 6: animate6 = false
+            case 7: animate7 = false
+            case 8: animate8 = false
+            case 9: animate9 = false
+            default: break
             }
         }
-        .padding(.vertical, Style.VerticalPadding.medium.rawValue)
-        .padding(.horizontal, Style.HorizontalPadding.medium.rawValue)
-        Spacer()
-        
     }
     
     private var button1: some View {
