8c3c00546576bb9c0b34bef93e089d01465483ad
barbaramartina <barbararodeker@gmail.com>
2024-08-25 14:28:21 +0200

    add tab view example for ios18

--- SwiftUICatalog/Views/Sections/Hierarchical Views/TabsView.swift
+++ SwiftUICatalog/Views/Sections/Hierarchical Views/TabsView.swift
@@ -41,57 +41,44 @@ struct TabsView: View, Comparable {
     @Environment(\.openURL) var openURL
     
     var body: some View {
-        
-        PageContainer(content:
-                        
-                        VStack(alignment: .leading) {
-            
-            DocumentationLinkView(link: "ttps://developer.apple.com/documentation/swiftui/tabview")
-            
-            intro
-            example1
-                .padding(.top, Style.HorizontalPadding.large.rawValue * 4)
-                .modifier(Divided())
-            
-            ContributedByView(name: "Ali Ghayeni H",
-                              link: "https://github.com/alighayeni")
-            .padding(.top, 80)
-            
-            
-        })
+        example1
     }
     
-    private var intro: some View {
-        Group {
-            Text("A view that switches between multiple child views using interactive user interface elements. \n\nTo create a user interface with tabs, place views in a TabView and apply the tabItem(_:) modifier to the contents of each tab. On iOS, you can also use one of the badge modifiers, like badge(_:), to assign a badge to each of the tabs. \n\nTo provide optional, extra information about a view, use a badge. The badge's text should be as brief as feasible. Only list rows, tab bars, and menus have badges.")
-                .fontWeight(.light)
-        }
-        
-    }
-    
+    @State private var customization = TabViewCustomization()
+    @State private var currentTab = 0
     private var example1: some View {
-        TabView {
-            Text("The First Tab")
-                .tabItem {
-                    Image(systemName: "1.square.fill")
-                    Text("First")
+        // for selection to work, do not forget to add "value", this was tricky and I could not find much examples about it
+        TabView(selection: $currentTab) {
+            Tab("First", systemImage: "1.square.fill", value: 0) {
+                Text("Content of the first tab")
+            }
+            .customizationID("tab.1")
+            Tab("Second", systemImage: "2.square.fill", value: 1) {
+                Text("Content of the second tab")
+            }
+            // this allow a tab or tab section to be customized (only in the style = sidebarAdaptable)
+            .customizationID("tab.2")
+            .badge("P")
+            Tab("Thrid", systemImage: "3.square.fill", value: 2) {
+                Text("Content of the third tab")
+            }
+            .customizationID("tab.3")
+            .badge(10)
+            // use a section to organize the content - in iOS that does not have any effect, but it does in iPad.
+            TabSection("Section") {
+                Tab("Tab section 1", systemImage: "3.square.fill", value: 5) {
+                    Text("Content - tab 1")
                 }
-            Text("Another Tab")
-                .tabItem {
-                    Image(systemName: "2.square.fill")
-                    Text("Second")
+                Tab("Tab section 2", systemImage: "3.square.fill", value: 6) {
+                    Text("Content - tab 2")
                 }
-                .badge(10)
-            Text("The Last Tab")
-                .tabItem {
-                    Image(systemName: "3.square.fill")
-                    Text("Third")
-                }
-                .badge("!")
+            }
+            // this prevent customization from happening
+            .customizationBehavior(.disabled, for: .sidebar, .tabBar)
         }
-        .font(.headline)
-        // iOS 18
-        //.tabViewStyle(.sidebarAdaptable)
+        // this will make the tab view adapt to the specific ios/ios/macOS/etc style...but also in iPads it allow the tab bar to be on the top when chosen by the user
+        .tabViewStyle(.sidebarAdaptable)
+        .tabViewCustomization($customization)
     }
 }
 
